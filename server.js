d50f1a7759087a2382c796c205fa7c00c79b7cda
Nitish Kumar Mahto <nitimahto503@gmail.com>
2026-04-06 22:21:54 +0530

    Server frontend from backend

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Simple test route
+// Serve frontend
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.send("Backend is running successfully!");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
