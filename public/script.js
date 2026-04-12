928db63de7f696f74bfb02d6e6f2f2b5315586a1
Nitish Kumar Mahto <nitimahto503@gmail.com>
2026-04-12 09:13:09 +0530

    Added some extra style

--- public/script.js
+++ public/script.js
@@ -2,7 +2,6 @@ const menu = document.getElementById("menu");
 const btn = document.getElementById("menuBtn");
 const overlay = document.getElementById("overlay");
 
-// MENU
 btn.onclick = () => {
   menu.classList.toggle("active");
   overlay.classList.toggle("show");
@@ -13,41 +12,11 @@ overlay.onclick = () => {
   overlay.classList.remove("show");
 };
 
-// NAVIGATION
-function goTo(page) {
-  window.location.href = page;
-}
-
-// 🔥 SLIDER
+// SLIDER
 let index = 0;
 const slides = document.getElementById("slides");
-const total = slides.children.length;
 
-const dotsContainer = document.getElementById("dots");
-
-// CREATE DOTS
-for (let i = 0; i < total; i++) {
-  const dot = document.createElement("span");
-  if (i === 0) dot.classList.add("active");
-
-  dot.onclick = () => {
-    index = i;
-    updateSlider();
-  };
-
-  dotsContainer.appendChild(dot);
-}
-
-function updateSlider() {
-  slides.style.transform = `translateX(-${index * 100}%)`;
-
-  document.querySelectorAll(".dots span").forEach((d, i) => {
-    d.classList.toggle("active", i === index);
-  });
-}
-
-// AUTO SLIDE
 setInterval(() => {
-  index = (index + 1) % total;
-  updateSlider();
+  index = (index + 1) % slides.children.length;
+  slides.style.transform = `translateX(-${index * 100}%)`;
 }, 3000);
